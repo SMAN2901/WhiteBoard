5e64784ee577ffeac91ca8a07a6ca13afe1e57c6
Sadman Rizwan <sadman.rizwan@gmail.com>
2020-01-09 01:25:48 +0600

    fixed preq form bug

--- src/components/form/prerequisiteform/PrerequisiteForm.jsx
+++ src/components/form/prerequisiteform/PrerequisiteForm.jsx
@@ -118,12 +118,8 @@ class PrerequisiteForm extends Form {
         const { index, prerequisites } = this.state;
 
         for (var k = 0; k < contents.length; k++) {
-            if (
-                contents[k].serial >= contents[index].serial ||
-                contents[k].preview
-            ) {
-                b.push(false);
-            } else b.push(true);
+            if (contents[k].serial >= contents[index].serial) b.push(false);
+            else b.push(true);
         }
 
         if (contents[index].prerequisites) {
@@ -175,14 +171,16 @@ class PrerequisiteForm extends Form {
                             className="preq-content-select"
                             onChange={this.onSelect}
                         >
-                            {contents.map(content => (
-                                <option
-                                    key={content.content_id}
-                                    value={content.content_id}
-                                >
-                                    {content.title}
-                                </option>
-                            ))}
+                            {contents
+                                .filter(i => !i.preview)
+                                .map(content => (
+                                    <option
+                                        key={content.content_id}
+                                        value={content.content_id}
+                                    >
+                                        {content.title}
+                                    </option>
+                                ))}
                         </select>
                         <label className="preq-preq-label">
                             Select prerequisite
